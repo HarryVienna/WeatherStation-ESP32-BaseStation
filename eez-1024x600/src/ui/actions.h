bcc84e4b588ac892d1d9ed8417156ffdcf5b3f44
Harald Kreuzer <mail@haraldkreuzer.net>
2026-01-22 21:20:38 +0100

    Add EEZ Studio files

---
+++ eez-1024x600/src/ui/actions.h
@@ -0,0 +1,31 @@
+#ifndef EEZ_LVGL_UI_EVENTS_H
+#define EEZ_LVGL_UI_EVENTS_H
+
+#include <lvgl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void action_event_setup_screen_loaded(lv_event_t * e);
+extern void action_event_wifi_scan(lv_event_t * e);
+extern void action_event_wifi_connect(lv_event_t * e);
+extern void action_event_weatherstation_start(lv_event_t * e);
+extern void action_event_text_area_app_id(lv_event_t * e);
+extern void action_event_text_area_password(lv_event_t * e);
+extern void action_event_text_area_latitude(lv_event_t * e);
+extern void action_event_text_area_longitude(lv_event_t * e);
+extern void action_event_text_area_hoehe(lv_event_t * e);
+extern void action_event_text_area_basis(lv_event_t * e);
+extern void action_event_text_area_sensor_name1(lv_event_t * e);
+extern void action_event_text_area_sensor_name2(lv_event_t * e);
+extern void action_event_text_area_sensor_name3(lv_event_t * e);
+extern void action_event_keyboard_text(lv_event_t * e);
+extern void action_event_keyboard_numeric(lv_event_t * e);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*EEZ_LVGL_UI_EVENTS_H*/
---
+++ eez-1024x600/src/ui/ui.c
@@ -0,0 +1,63 @@
+#if defined(EEZ_FOR_LVGL)
+#include <eez/core/vars.h>
+#endif
+
+#include "ui.h"
+#include "screens.h"
+#include "images.h"
+#include "actions.h"
+#include "vars.h"
+
+
+
+
+
+
+
+#if defined(EEZ_FOR_LVGL)
+
+void ui_init() {
+    eez_flow_init(assets, sizeof(assets), (lv_obj_t **)&objects, sizeof(objects), images, sizeof(images), actions);
+}
+
+void ui_tick() {
+    eez_flow_tick();
+    tick_screen(g_currentScreen);
+}
+
+#else
+
+static int16_t currentScreen = -1;
+
+static lv_obj_t *getLvglObjectFromIndex(int32_t index) {
+    if (index == -1) {
+        return 0;
+    }
+    return ((lv_obj_t **)&objects)[index];
+}
+
+static const void *getLvglImageByName(const char *name) {
+    for (size_t imageIndex = 0; imageIndex < sizeof(images) / sizeof(ext_img_desc_t); imageIndex++) {
+        if (strcmp(images[imageIndex].name, name) == 0) {
+            return images[imageIndex].img_dsc;
+        }
+    }
+    return 0;
+}
+
+void loadScreen(enum ScreensEnum screenId) {
+    currentScreen = screenId - 1;
+    lv_obj_t *screen = getLvglObjectFromIndex(currentScreen);
+    lv_scr_load_anim(screen, LV_SCR_LOAD_ANIM_FADE_IN, 200, 0, false);
+}
+
+void ui_init() {
+    create_screens();
+    loadScreen(SCREEN_ID_SETUP_SCREEN);
+}
+
+void ui_tick() {
+    tick_screen(currentScreen);
+}
+
+#endif
